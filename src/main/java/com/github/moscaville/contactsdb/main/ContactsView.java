3ba7043c74a6c6178f87912b20cfb8bfc560ca82
Carl J. Mosca <carljmosca@gmail.com>
2015-09-05 21:32:56 -0400

    added filter

--- src/main/java/com/github/moscaville/contactsdb/main/ContactsView.java
+++ src/main/java/com/github/moscaville/contactsdb/main/ContactsView.java
@@ -6,18 +6,18 @@
 package com.github.moscaville.contactsdb.main;
 
 import com.github.moscaville.contactsdb.Sections;
+import com.vaadin.data.Property;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.ViewScope;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.PostConstruct;
 import org.vaadin.spring.sidebar.annotation.FontAwesomeIcon;
 import org.vaadin.spring.sidebar.annotation.SideBarItem;
@@ -40,41 +40,57 @@ public class ContactsView extends CssLayout implements View {
     final Panel pnlMain = new Panel();
     VerticalLayout vLayout = new VerticalLayout();
     HorizontalLayout hLayout = new HorizontalLayout();
-    HorizontalLayout vButtons = new HorizontalLayout();
-    Button btnDuplicate = new Button("Duplicate");
-    private final List<ContactDetailView> contactDetailViews;
+    HorizontalLayout vControls = new HorizontalLayout();
+    private Button btnEdit;
+    private Button btnDuplicate;    
+    private TextField tfFilter;
 
     public ContactsView() {
-        contactDetailViews = new ArrayList<>();
     }
 
     @PostConstruct
     void init() {
 
+        btnEdit = new Button("Edit");
+        btnDuplicate = new Button("Duplicate");
+        tfFilter = new TextField();
+        tfFilter.setImmediate(true);
+        tfFilter.setInputPrompt("Filter");
+        
         vLayout.setMargin(true);
-        vButtons.setMargin(true);
-        vButtons.addComponent(btnDuplicate);
+        vControls.setMargin(true);
+        vControls.addComponent(tfFilter);
+        vControls.addComponent(btnEdit);
+        vControls.addComponent(btnDuplicate);
         hLayout.setSizeFull();
 
         contactTable = new ContactTable();
         vLayout.addComponent(contactTable);
         vLayout.setHeight("75%");
-        addComponent(vButtons);
+        addComponent(vControls);
         addComponent(vLayout);
         setSizeFull();
+        
+        tfFilter.addValueChangeListener((Property.ValueChangeEvent event) -> {
+            
+        });
 
+        btnEdit.addClickListener((Button.ClickEvent event) -> {
+            
+        });
+        
         btnDuplicate.addClickListener((Button.ClickEvent event) -> {
 //            ContactDetailView v = new ContactDetailView();
 //            contactDetailViews.add(v);
 //            v.setContactId(contactDetailViews.size());
 //            vLayout.addComponent(v);
         });
-
+        
     }
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
 }
