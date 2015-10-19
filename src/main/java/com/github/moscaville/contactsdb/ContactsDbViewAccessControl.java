8f72036d32a244137d54a3a9add96b337994b2d5
Carl J. Mosca <carljmosca@gmail.com>
2015-10-18 22:28:36 -0400

    added oauth logic

---
+++ src/main/java/com/github/moscaville/contactsdb/ContactsDbViewAccessControl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.moscaville.contactsdb;
+
+import com.vaadin.spring.access.ViewAccessControl;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.ui.UI;
+
+/**
+ *
+ * @author moscac
+ */
+@SpringComponent
+public class ContactsDbViewAccessControl implements ViewAccessControl {
+
+    @Override
+    public boolean isAccessGranted(UI ui, String viewName) {
+        if ("mainView".equals(viewName))
+            return true;
+        return ValoSideBarUI.get().isLoggedIn();
+    }
+    
+}
